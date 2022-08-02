918a9179f28343ee702f9279033296b3e6c439e9
fengbujue <1125238900@qq.com>
2022-08-02 12:21:51 +0800

    完成了登录权限设置

---
+++ src/permission.js
@@ -0,0 +1,33 @@
+import router from './router'
+import store from '@/store'
+// 路由全局前置守卫
+// 会在所有路由进入之前触发
+// to: 去哪里的路由信息
+// from: 来自于哪个路由的信息
+// next: 是否进入
+const whiteList = ['/404', '/login']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  // 1.登录
+  if (token) {
+    // 是否进入登录页
+    if (to.path === '/login') {
+      // 1.1 是 跳转到首页
+      next('/')
+    } else {
+      // 1.2 不是 直接进入
+      next()
+    }
+  } else {
+    // 2. 未登录
+    const isCludes = whiteList.includes(to.path)
+    // 访问的是否在白名单（未登录也能访问的页面）
+    if (isCludes) {
+      // 2.1 在白名单， 放行
+      next()
+    } else {
+      // 2.2 不在白名单（不登录不能访问）跳到登录页
+      next('/login')
+    }
+  }
+})
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -53,7 +53,7 @@ export default {
     return {
       // 定义数据
       loginForm: {
-        mobile: '13800000002',
+        mobile: '13800000004',
         password: '123456'
       },
       loginFormRules: {
